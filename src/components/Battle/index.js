078796e9e3f0d4a82f65228c87d2edc10793e78d
Chiwon <emphasize1@gmail.com>
2021-07-05 22:03:43 +0900

    Add: add input boxes

--- src/components/Battle/index.js
+++ src/components/Battle/index.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Battle() {
-  return <h1 className="center-text">This is Battle!</h1>;
+  const [text, setText] = useState("");
+
+  function onChange(event) {
+    setText(event.target.value);
+  }
+
+  function onClick(event) {
+    event.preventDefault();
+    console.log("event.target.value", event.target.value);
+  }
+
+  return (
+    <div>
+      <h1 className="center-text">This is Battle!</h1>
+      <form>
+        <input placeholder="User 1" onChange={onChange}/>
+        <br></br>
+        <input placeholder="User 2" onChange={onChange}/>
+        <button onClick={onClick}>입력</button>
+      </form>
+    </div>
+  );
 }
